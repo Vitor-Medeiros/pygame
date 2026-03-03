c4e5116b699a10ad458e20ea515f7f56d105bbdf
Vitor-Medeiros <vitorbortot@gmail.com>
2026-03-03 19:35:13 -0300

    Ódio

--- teste-pygame.py
+++ teste-pygame.py
@@ -4,27 +4,29 @@ import random
 
 pygame.init()
 
+
 largura = 800
 altura = 600
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Janela')
+pygame.display.set_caption('janela')
 
 PRETO = (0,0,0)
+
 BRANCO = (255,255,255)
 
-tamanho_fonte = 50
+tamanho_fonte = 45
 fonte = pygame.font.SysFont(None, tamanho_fonte)
 
-texto = fonte.render('Mateus', True, BRANCO)
-texto_2 = fonte.render('Bringhenti', True, BRANCO)
+texto = fonte.render('Vitor', True, BRANCO)
+novoTexto = fonte.render('Bortot', True, BRANCO)
 
 texto_rect = texto.get_rect()
 texto_rect.x = random.randint(0, largura - texto_rect.width)
 texto_rect.y = random.randint(0, altura - texto_rect.height)
 
-texto_2_rect = texto_2.get_rect()
-texto_2_rect.x = random.randint(0, largura - texto_2_rect.width)
-texto_2_rect.y = random.randint(0, altura - texto_2_rect.height)
+novoTexto_rect = novoTexto.get_rect()
+novoTexto_rect.x = random.randint(0, largura - novoTexto_rect.width)
+novoTexto_rect.y = random.randint(0, altura - novoTexto_rect.height)
 
 clock = pygame.time.Clock()
 
@@ -54,8 +56,8 @@ while rodando:
     texto_rect.x += velocidade_x1
     texto_rect.y += velocidade_y1
 
-    texto_2_rect.x += velocidade_x2
-    texto_2_rect.y += velocidade_y2
+    novoTexto_rect.x += velocidade_x2
+    novoTexto_rect.y += velocidade_y2
 
     if texto_rect.right >= largura:
         texto_rect.right = largura
@@ -81,31 +83,31 @@ while rodando:
         velocidade_y1 = random.randint(-2,0)
         bateu = True
 
-    if texto_2_rect.right >= largura:
-        texto_2_rect.right = largura
+    if novoTexto_rect.right >= largura:
+        novoTexto_rect.right = largura
         velocidade_x2 = random.randint(-2,0)
         velocidade_y2 = random.randint(-2,2)
         bateu = True
 
-    if texto_2_rect.left <= 0:
-        texto_2_rect.left = 0
+    if novoTexto_rect.left <= 0:
+        novoTexto_rect.left = 0
         velocidade_x2 = random.randint(0,2)
         velocidade_y2 = random.randint(-2,2)
         bateu = True
 
-    if texto_2_rect.top <= 0:
-        texto_2_rect.top = 0
+    if novoTexto_rect.top <= 0:
+        novoTexto_rect.top = 0
         velocidade_x2 = random.randint(-2,2)
         velocidade_y2 = random.randint(0,2)
         bateu = True
 
-    if texto_2_rect.bottom >= altura:
-        texto_2_rect.bottom = altura
+    if novoTexto_rect.bottom >= altura:
+        novoTexto_rect.bottom = altura
         velocidade_x2 = random.randint(-2,2)
         velocidade_y2 = random.randint(-2,0)
         bateu = True
 
-    if texto_rect.colliderect(texto_2_rect):
+    if texto_rect.colliderect(novoTexto_rect):
         velocidade_x1 = random.randint(-2,2)
         velocidade_y1 = random.randint(-2,2)
         velocidade_x2 = random.randint(-2,2)
@@ -118,11 +120,11 @@ while rodando:
             random.randint(1,255),
             random.randint(1,255)
         )
-        texto = fonte.render('Mateus', True, cor_atual)
-        texto_2 = fonte.render('Bringhenti', True, cor_atual)
+        texto = fonte.render('Vitor', True, cor_atual)
+        novoTexto = fonte.render('Bortot', True, cor_atual)
 
     tela.blit(texto, texto_rect)
-    tela.blit(texto_2, texto_2_rect)
+    tela.blit(novoTexto, novoTexto_rect)
 
     clock.tick(240)
     pygame.display.flip()
